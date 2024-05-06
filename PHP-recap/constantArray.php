c64c6499d27fb0d44a7a6722e5422e76c493cd93
DaryllCulas <culasdaryll99@gmail.com>
2024-05-07 03:47:43 +0800

    Added

---
+++ PHP-recap/constantArray.php
@@ -0,0 +1,12 @@
+<?php
+
+define('Capitals', array(
+  'USA' => 'Washington DC',
+  'Canada' => 'Ottawa',
+  'UK' => 'London',
+  'France' => 'Paris',
+  'Japan' => 'Tokyo',
+  'China' => 'Beijing',
+));
+
+print_r(Capitals);
