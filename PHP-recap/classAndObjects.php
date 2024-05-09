01bbe865c28f512dd3a1d8326e00cf07dc77668e
DaryllCulas <culasdaryll99@gmail.com>
2024-05-09 20:48:25 +0800

    Added

---
+++ PHP-recap/classAndObjects.php
@@ -0,0 +1,80 @@
+<?php
+
+class Books
+{
+
+  public $title;
+  public $author;
+  public $price;
+
+
+  function __construct($bookTitle, $bookAuthor, $bookPrice)
+  {
+    $this->title =  $bookTitle;
+    $this->author = $bookAuthor;
+    $this->price = $bookPrice;
+  }
+
+
+  function setTitle($bookTitle)
+  {
+    $this->title = $bookTitle;
+  }
+
+  function setAuthor($bookAuthor)
+  {
+    $this->author = $bookAuthor;
+  }
+
+  function setPrice($bookPrice)
+  {
+    $this->price = $bookPrice;
+  }
+
+  function getTitle()
+  {
+    echo $this->title . PHP_EOL;
+  }
+
+  function getAuthor()
+  {
+    echo $this->author . PHP_EOL;
+  }
+
+  function getPrice()
+  {
+    echo $this->price . PHP_EOL;
+  }
+}
+
+
+$physics = new Books("Physics", "Albert Einstein", 100);
+$math = new Books("Math", "Nikola Tesla", 200);
+$chemistry = new Books("Chemistry", "Marie Curie", 300);
+
+// $physics->setTitle('Physics');
+// $math->setTitle('Math');
+// $chemistry->setTitle('Chemistry');
+
+
+// $physics->setAuthor('Albert Einstein');
+// $math->setAuthor('Nikola Tesla');
+// $chemistry->setAuthor('Marie Curie');
+
+
+// $physics->setPrice(100);
+// $math->setPrice(200);
+// $chemistry->setPrice(300);
+
+$physics->getTitle();
+$math->getTitle();
+$chemistry->getTitle();
+
+$physics->getAuthor();
+$math->getAuthor();
+$chemistry->getAuthor();
+
+
+$physics->getPrice();
+$math->getPrice();
+$chemistry->getPrice();
