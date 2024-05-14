a123dead6b9747e18ff8b41f07bf341dc66169ce
DaryllCulas <culasdaryll99@gmail.com>
2024-05-15 04:25:19 +0800

    Added

---
+++ PHP-recap/abstractClass.php
@@ -0,0 +1,26 @@
+<?php
+
+
+abstract class marks
+{
+  protected int $m1, $m2, $m3;
+  abstract protected function percent(): float;
+}
+
+class student extends marks
+{
+  public function __construct($x, $y, $z)
+  {
+    $this->m1 = $x;
+    $this->m2 = $y;
+    $this->m3 = $z;
+  }
+
+  public function percent(): float
+  {
+    return ($this->m1 + $this->m2 + $this->m3) * 100 / 300;
+  }
+}
+
+$s1 = new student(70, 80, 90);
+echo "The average of marks: " . $s1->percent() . "%" . PHP_EOL;
