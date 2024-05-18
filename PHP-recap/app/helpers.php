029e067e1fed61fe3d3fc0569632ad8ea6e97631
DaryllCulas <culasdaryll99@gmail.com>
2024-05-19 03:31:08 +0800

    Add file and make function for formatDollarAmount

---
+++ PHP-recap/app/helpers.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+function formatDollarAmount(float $amount): string
+{
+
+  $isNegative = $amount < 0;
+
+  return ($isNegative ? '-' : '') . '$' . number_format(abs($amount), 2);
+}
